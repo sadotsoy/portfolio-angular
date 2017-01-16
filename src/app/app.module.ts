bed40c72aa42211c408e04f12cd6158e0667880f
SadotCortes <smcortes@iM-L13-293.local>
2017-01-16 13:04:36 -0600

    Router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { SadotComponent } from './sadot/sadot.component';
-import { LeftBarComponent } from './left-bar/left-bar.component';
-import { BodyComponent } from './body/body.component';
+import {AppComponent} from './app.component';
+import {NavBarComponent} from './nav-bar/nav-bar.component';
+import {SadotComponent} from './sadot/sadot.component';
+import {LeftBarComponent} from './left-bar/left-bar.component';
+import {BodyComponent} from './body/body.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent,
-    SadotComponent,
-    LeftBarComponent,
-    BodyComponent
+    AppComponent, NavBarComponent, SadotComponent, LeftBarComponent, BodyComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    NgbModule.forRoot()
+    BrowserModule, FormsModule, HttpModule, NgbModule.forRoot(),
+    RouterModule.forRoot([
+      {
+        path: 'about',
+        component: SadotComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
